51851507015f6c09e7dbb818c1e0695365fd760b
Md Emamul Murshalin <emamul727@gmail.com>
2021-09-26 18:21:40 +0600

    authinticate middleware use

--- routes/admin/frontEnd.php
+++ routes/admin/frontEnd.php
@@ -2,18 +2,23 @@
 
 //Employee
 use App\Http\Controllers\FrontEnd\FrontEndController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/employee/list/view', [FrontEndController::class, 'employeeListView'])
-    ->name('employee.list_view');
 
-Route::get('/soft-delete-employees', [FrontEndController::class, 'trashEmployeeListView'])
-    ->name('employee.trash_list_view');
+Route::middleware([Authenticate::class])->group(function () {
+    Route::get('/employee/list/view', [FrontEndController::class, 'employeeListView'])
+        ->name('employee.list_view');
+
+    Route::get('/soft-delete-employees', [FrontEndController::class, 'trashEmployeeListView'])
+        ->name('employee.trash_list_view');
 
 //User
-Route::get('/user/list/view', [FrontEndController::class, 'userListView'])
-    ->name('user.list_view');
+    Route::get('/user/list/view', [FrontEndController::class, 'userListView'])
+        ->name('user.list_view');
 
 //Roles
-Route::get('/role/list/view', [FrontEndController::class, 'roleListView'])
-    ->name('role.list_view');
+    Route::get('/role/list/view', [FrontEndController::class, 'roleListView'])
+        ->name('role.list_view');
+});
+
